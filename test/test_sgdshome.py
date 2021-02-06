f175d277d0a703972c027e1e45362f6fd045bf4c
isaacteh <isaacteh@ust-global.com>
2021-02-07 00:12:43 +0800

    update driver manager

--- test/test_sgdshome.py
+++ test/test_sgdshome.py
@@ -11,7 +11,7 @@ from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
 from webdriver_manager.chrome import ChromeDriverManager
 
-driver = webdriver.Chrome(ChromeDriverManager().install())
+driver = webdriver.Chrome()
 vars = {}
   
 driver.get("https://neat-birch-a5e6b.netlify.app/")
