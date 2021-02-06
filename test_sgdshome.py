da676811fc247550745b7690808d8f41ea346c1f
isaacteh <isaacteh@ust-global.com>
2021-02-06 23:28:31 +0800

    update

--- test_sgdshome.py
+++ test_sgdshome.py
@@ -11,7 +11,7 @@ from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
 
 
-driver = webdriver.Chrome('/usr/local/bin/chromedriver')
+driver = webdriver.Chrome()
 vars = {}
   
 driver.get("https://neat-birch-a5e6b.netlify.app/")
