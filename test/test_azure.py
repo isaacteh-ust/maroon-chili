98849cdb118caf535952ac82ef975eb7afdaba23
isaacteh <isaacteh@ust-global.com>
2021-02-20 01:37:32 +0800

    update

---
+++ test/test_azure.py
@@ -0,0 +1,33 @@
+"""
+Test things in Azure Pipelines
+"""
+
+import os
+import pytest
+
+
+def test_attachment(add_nunit_attachment):
+    """
+    Test adding custom attachment
+    """
+    path = os.path.join(os.path.abspath(os.path.dirname(__file__)), "home.png")
+    add_nunit_attachment(path, "PBJT")
+    assert 1 == 1
+
+
+def test_property(record_nunit_property):
+    """
+    Test adding custom property
+    """
+    record_nunit_property("test", "value")
+    assert 1 == 1
+
+
+@pytest.mark.xfail
+def test_failure():
+    assert 1 == 0
+
+
+@pytest.mark.skip("Example skip")
+def test_skip():
+    assert 1 == 0
