0b330205ce4268fdf2b439bc27783556e97d863a
isaacteh <isaacteh@ust-global.com>
2021-02-20 14:59:20 +0800

    test py

--- test/chili-test.py
+++ test/chili-test.py
@@ -39,29 +39,17 @@ class TestTest():
     path = os.path.join(os.path.abspath(os.path.dirname(__file__)), "getstarted.png")
     add_nunit_attachment(path, "getstarted")
 
-  def screen_shot(self):
-      """Take a Screen-shot of the drive homepage, when it Failed."""
-      for method, error in self._outcome.errors:
-          if error:
-              self.driver.get_screenshot_as_file("screenshot" + self.id() + ".png")
-
-  def test_demo(self):
-      """A test case that fails because of missing element."""
-      self.driver.get("http://www.google.com")
-      self.driver.find_element_by_css_selector("div.that-does-not-exist")
-      
-
-  def test_samples(self, add_nunit_attachment):
-    try:
-        self.driver.set_window_size(1440, 795)
-        self.driver.get("https://maroon-chili-27bd3.netlify.app/")
-        elements = self.driver.find_elements(By.NAME, "sample")
-        assert elements == "sample"
-        self.driver.save_screenshot("test/sample.png")
-        path = os.path.join(os.path.abspath(os.path.dirname(__file__)), "sample.png")
-        add_nunit_attachment(path, "sample")
-    except Exception as e:
-        raise
+#  def test_samples(self, add_nunit_attachment):
+#    try:
+#        self.driver.set_window_size(1440, 795)
+#        self.driver.get("https://maroon-chili-27bd3.netlify.app/")
+#        elements = self.driver.find_elements(By.NAME, "sample")
+#        assert elements == "sample"
+#        self.driver.save_screenshot("test/sample.png")
+#        path = os.path.join(os.path.abspath(os.path.dirname(__file__)), "sample.png")
+#        add_nunit_attachment(path, "sample")
+#    except Exception as e:
+#        raise
 
   def test_blog(self, add_nunit_attachment):
     self.driver.set_window_size(1440, 795)
