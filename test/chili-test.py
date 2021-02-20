96fceca3563bbd39ddfc6c2dba93f8a507057496
isaacteh <isaacteh@ust-global.com>
2021-02-20 13:03:56 +0800

    add test case

--- test/chili-test.py
+++ test/chili-test.py
@@ -16,16 +16,32 @@ class TestTest():
   def setup_method(self, method):
     chrome_options = Options()  
     chrome_options.add_argument("--headless")  
-    self.driver = webdriver.Chrome(chrome_options=chrome_options)
+    chrome_options.add_argument('--ignore-certificate-errors')
+    self.driver = webdriver.Chrome(options=chrome_options)
     self.vars = {}
   
   def teardown_method(self, method):
     self.driver.quit()
   
-  def test_test(self, add_nunit_attachment):
+  def test_homepage(self, add_nunit_attachment):
+    self.driver.set_window_size(1440, 795)
     self.driver.get("https://maroon-chili-27bd3.netlify.app/")    
-    print('open the site')
     self.driver.save_screenshot("test/home.png")
     path = os.path.join(os.path.abspath(os.path.dirname(__file__)), "home.png")
-    print(path)
-    add_nunit_attachment(path, "peanut butter jelly time")
+    add_nunit_attachment(path, "home")
+
+  def test_getstarted(self, add_nunit_attachment):
+    self.driver.set_window_size(1440, 795)
+    self.driver.get("https://maroon-chili-27bd3.netlify.app/")
+    self.driver.find_element(By.LINK_TEXT, "Get Started").click()
+    self.driver.save_screenshot("test/getstarted.png")
+    path = os.path.join(os.path.abspath(os.path.dirname(__file__)), "getstarted.png")
+    add_nunit_attachment(path, "getstarted")
+
+  def test_blog(self, add_nunit_attachment):
+    self.driver.set_window_size(1440, 795)
+    self.driver.get("https://maroon-chili-27bd3.netlify.app/")
+    self.driver.find_element(By.LINK_TEXT, "Blog").click()
+    self.driver.save_screenshot("test/blog.png")
+    path = os.path.join(os.path.abspath(os.path.dirname(__file__)), "blog.png")
+    add_nunit_attachment(path, "blog")
